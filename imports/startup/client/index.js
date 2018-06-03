72b822f5c87501d1dd0bdaf1eb39b1a65589b7c8
abhishek71994 <abhishek71994@gmail.com>
2018-06-03 16:50:24 +0530

    graphiql setup

--- imports/startup/client/index.js
+++ imports/startup/client/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
 
-const App = () => <h1>App is running</h1>
+import App from '../../ui/App';
 
 Meteor.startup(()=>{
 	render(<App />,document.getElementById("app"))
