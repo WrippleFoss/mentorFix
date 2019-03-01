df914d0b9b5417b833ccc4fd1b44c92a2ca789c6
Palash Gupta <palashg7563@gmail.com>
2019-03-02 00:32:44 +0530

    app and app routes are created

--- view-src/src/App.js
+++ view-src/src/App.js
@@ -1,13 +1,28 @@
-import React, { Component } from 'react'
+import React from 'react';
+import { Provider } from 'react-redux';
+import BrowserRouter from "react-router-dom/BrowserRouter"
 
-class App extends Component {
-  render () {
-    return (
-      <div>
-        <h1>Hello world</h1>
-      </div>
-    )
-  }
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+import AppRoutes from './AppRoutes';
+
+import theme from './theme';
+import configureStore from './store';
+
+
+function App() {
+  return (
+    <CssBaseline>
+      <Provider store={configureStore(window.__INITIAL_STATE__)}>
+        <BrowserRouter>
+          <MuiThemeProvider theme={theme}>
+            <AppRoutes />
+          </MuiThemeProvider>
+        </BrowserRouter>
+      </Provider>
+    </CssBaseline>
+  );
 }
 
-export default App
+export default App;
