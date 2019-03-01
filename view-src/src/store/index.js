0b8df908be1c408370bb55f8981539cf9f90cc67
Palash Gupta <palashg7563@gmail.com>
2019-03-02 00:30:07 +0530

    store is created

---
+++ view-src/src/store/index.js
@@ -0,0 +1,24 @@
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+import rootReducers from '../reducers';
+
+const middleware = [thunk];
+
+const composeEnhancers = composeWithDevTools({});
+
+const enhancers = composeEnhancers(
+  applyMiddleware(...middleware),
+);
+
+const configStore = (initialState) => {
+  const store = createStore(
+    rootReducers,
+    initialState,
+    enhancers,
+  );
+
+  return store;
+};
+
+export default configStore;
