21bf29547c9ab9b72b3a4e53a816064ec8eda521
Palash Gupta <palashg7563@gmail.com>
2019-03-06 20:40:36 +0530

    linting fix

--- view-src/src/App.js
+++ view-src/src/App.js
@@ -1,28 +1,27 @@
-import React from 'react';
-import { Provider } from 'react-redux';
-import BrowserRouter from "react-router-dom/BrowserRouter"
+import React from "react";
+import { Provider } from "react-redux";
+import BrowserRouter from "react-router-dom/BrowserRouter";
 
-import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
-import CssBaseline from '@material-ui/core/CssBaseline';
+import MuiThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
+import CssBaseline from "@material-ui/core/CssBaseline";
 
-import AppRoutes from './AppRoutes';
+import AppRoutes from "./AppRoutes";
 
-import theme from './theme';
-import configureStore from './store';
-
+import theme from "./theme";
+import configureStore from "./store";
 
 function App() {
-  return (
-    <CssBaseline>
-      <Provider store={configureStore(window.__INITIAL_STATE__)}>
-        <BrowserRouter>
-          <MuiThemeProvider theme={theme}>
-            <AppRoutes />
-          </MuiThemeProvider>
-        </BrowserRouter>
-      </Provider>
-    </CssBaseline>
-  );
+	return (
+		<CssBaseline>
+			<Provider store={configureStore(window.__INITIAL_STATE__)}>
+				<BrowserRouter>
+					<MuiThemeProvider theme={theme}>
+						<AppRoutes />
+					</MuiThemeProvider>
+				</BrowserRouter>
+			</Provider>
+		</CssBaseline>
+	);
 }
 
 export default App;
--- view-src/src/actions/app.js
+++ view-src/src/actions/app.js
@@ -1,11 +1,11 @@
-import { APP_IS_READY, APP_IS_NOT_READY } from '../constant/app';
+import { APP_IS_READY, APP_IS_NOT_READY } from "../constant/app";
 
-export const AppIsReady = (payload) => ({
-  type: APP_IS_READY,
-  payload,
+export const AppIsReady = payload => ({
+	type: APP_IS_READY,
+	payload
 });
 
-export const AppIsNotReady = (payload) => ({
-  type: APP_IS_NOT_READY,
-  payload,
+export const AppIsNotReady = payload => ({
+	type: APP_IS_NOT_READY,
+	payload
 });
