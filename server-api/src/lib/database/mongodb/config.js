6323956ab900547ac235e2c7b71709692aadc52d
Palash Gupta <palashg7563@gmail.com>
2019-03-16 02:51:35 +0530

    project structuring is done and mongodb is added

---
+++ server-api/src/lib/database/mongodb/config.js
@@ -0,0 +1,8 @@
+/* eslint-disable import/prefer-default-export */
+
+/**
+ * @todo
+ * make the difference reference for the db url in the different enviroment
+ */
+
+export const DB_URL = 'mongodb://localhost:27017/mentorfix-dev';
