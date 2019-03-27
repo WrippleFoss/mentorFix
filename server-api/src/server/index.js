d25a430f83fa16624e5f699a467ce1e673ca8957
Palash Gupta <palashg7563@gmail.com>
2019-03-27 18:27:12 +0530

    server is configured for production & development

--- server-api/src/server/index.js
+++ server-api/src/server/index.js
@@ -1,16 +1,8 @@
-import express from 'express';
+const { NODE_ENV } = process.env;
 
-import middleware from './middleware';
-import { PORT } from './config';
-import MongoDb from '../lib/database/mongodb';
-
-// app is initialized
-const app = express();
-
-// all the middleware are defined here
-middleware(app);
-
-// server is started
-app.listen(PORT, () => {
-  console.log(`The server is listening on ${PORT}`);
-});
+if (NODE_ENV === "development") {
+  module.exports = require("./server.dev");
+}
+if (NODE_ENV === "production") {
+  module.exports = require("./server.pro");
+}
