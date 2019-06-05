0b2ed6a70d7f2be83fc06041d758f0c7490bb0d6
abhishek71994 <abhishek71994@gmail.com>
2019-06-05 20:19:18 +0800

    file structure change src-view

--- view-src/src/constant/app.js
+++
@@ -1,2 +0,0 @@
-export const APP_IS_READY = "APP_IS_READY";
-export const APP_IS_NOT_READY = "APP_IS_NOT_READY";
--- view-src/src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import App from "./App";
-
-const reducer = combineReducers({
-    App
-});
-
-export default reducer;
