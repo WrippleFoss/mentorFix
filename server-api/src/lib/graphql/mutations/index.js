bcf71da8900913834cbaf0fe8eb63d7dfba86451
abhishek71994 <abhishek71994@gmail.com>
2019-06-06 21:14:39 +0800

    Modularized the mentor mutation

--- server-api/src/lib/graphql/mutations/index.js
+++ server-api/src/lib/graphql/mutations/index.js
@@ -1,24 +1,10 @@
-import { GraphQLObjectType, GraphQLString } from 'graphql';
-import mentorType from '../query/types/mentorType';
-import mentorModel from '../../database/models/mentor';
+import { GraphQLObjectType } from 'graphql';
+import AddMentor from './mentor/addMentor';
 
 const rootMutation = new GraphQLObjectType({
   name: 'RootMutation',
   fields: {
-    addMentor: {
-      type: mentorType,
-      args: {
-        email: { type: GraphQLString },
-        password: { type: GraphQLString }
-      },
-      resolve(parent, args) {
-        let mentor = new mentorModel({
-          email: args.email,
-          password: args.password
-        });
-        return mentor.save();
-      }
-    }
+    addMentor: AddMentor
   }
 });
 
