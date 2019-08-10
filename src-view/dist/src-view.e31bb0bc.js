acc57c3990dacc0346e372593c2f031a861cf25f
abhishek71994 <abhishek71994@gmail.com>
2019-08-10 19:09:13 +0800

    added global variables for styling

--- src-view/dist/src-view.e31bb0bc.js
+++ src-view/dist/src-view.e31bb0bc.js
@@ -70623,7 +70623,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54441" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62522" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
