10efeb844a2566e9a7dc4fff3365492a0502aa00
nvs16 <nvsabhishek.india@gmail.com>
2020-03-04 02:19:01 +0530

    added test for login button test

--- src-view/specs/loginview.test.js
+++ src-view/specs/loginview.test.js
@@ -1,8 +1,13 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 import LoginView from '../views/LoginView'
+import { Form, Button } from "semantic-ui-react";
 describe('First React component test with Enzyme', () => {
   it('renders without crashing', () => {
     shallow(<LoginView />)
+  })
+  it('has correct login button text', () => {
+    const wrapper = shallow(<LoginView />)
+    expect(wrapper.find(Form).find(Button).prop('content')).toEqual("Log In");
   })
 })
