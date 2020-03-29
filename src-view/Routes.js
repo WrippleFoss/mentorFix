275bb87aebb3c4b095e80d6b64896763257b4d92
Mallika <mallika2011@yahoo.com>
2020-03-29 14:30:03 +0530

    Frontend for login-signup screens added

--- src-view/Routes.js
+++ src-view/Routes.js
@@ -2,14 +2,18 @@ import React from 'react'
 import LandingView from './views/LandingView'
 import { Router } from '@reach/router'
 import LoginView from './views/LoginView'
+import Transition from './views/Transition'
+// import Login from './views/Login'
+// import Signup from './views/Signup'
 
 import SignUpView from './views/SignUpView'
 export default () => {
   return (
     <Router>
       <LandingView path='/' />
-      <LoginView path='/login' />
-      <SignUpView path='/sign-up' />
+      <Transition path='/login-signup' />
+      {/* <Login path='/login' /> */}
+      {/* <Signup path='/sign-up' /> */}
     </Router>
   )
 }
