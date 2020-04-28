c32f07d8f378186e736017b3e92705f734153404
NikitaMasand <masandnikita@gmail.com>
2020-04-28 13:25:35 +0530

    login screen front end issue

--- src-view/views/LoginView.jsx
+++ src-view/views/LoginView.jsx
@@ -1,23 +1,28 @@
 import React from "react";
 import { Form, Button } from "semantic-ui-react";
 import styles from "../assets/styles/LoginView.module.scss";
-import AppLayout from "../layouts/AppLayout";
+import NavbarLogin from "../components/NavbarLogin";
+import topleft from "../assets/images/topleft.png";
+import bottomright from "../assets/images/bottomright.png";
 const LoginForm = () => {
   return (
     <>
-      <AppLayout>
+      <NavbarLogin />
+        <div className={styles.loginpage}>
+        <div className={styles.topleft}><img src={topleft} alt="mentorfix" /></div>
         <div className={styles.loginDivWrapper}>
           <div className={styles.loginDiv}>
-            <h1>Login to continue</h1>
+            <h1>Login</h1>
             <Form className={styles.loginForm}>
-              <Form.Input fluid label="Email" placeholder="Enter your email" />
-              <Form.Input fluid label="Password" placeholder="Enter password" />
-              <Button positive content="Log In" />
+              <Form.Input fluid label="Email" placeholder="Enter your email address" />
+              <Form.Input fluid label="Password" placeholder="Enter your password" />
+              <Button content="Log In" />
             </Form>
-            <a>Forgot password?</a>
+            <div className={styles.forgotcontent}>Forgot password? <a>Click here</a></div>
           </div>
         </div>
-      </AppLayout>
+        <div className={styles.bottomright}><img src={bottomright} alt="mentorfix" /></div>
+        </div>
     </>
   );
 };
